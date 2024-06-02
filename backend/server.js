eb66a381ff03f7118cdeb753028845c5d3fdaefa
Soumyadeep Mondal <142522496+soumyadeep1143@users.noreply.github.com>
2024-06-03 01:58:10 +0530

    haha

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,16 @@ import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.route.js";
 import connectMongoDB from './db/connectMongoDB.js'
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 
 app.use("/api/auth", authRoutes);
