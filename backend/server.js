015fc7658e68e273b781fd32e55044ae1e3ab052
Soumyadeep Mondal <142522496+soumyadeep1143@users.noreply.github.com>
2024-06-03 21:27:41 +0530

    guess backend done

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import { v2 as cloudinary } from "cloudinary";
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
 
 import connectMongoDB from './db/connectMongoDB.js';
 
@@ -28,6 +29,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 
 
 
