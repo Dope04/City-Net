e9317dd8d1c15e5bf4134042593eac10e94c4509
Soumyadeep Mondal <142522496+soumyadeep1143@users.noreply.github.com>
2024-06-03 22:10:07 +0530

    assets

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import { v2 as cloudinary } from "cloudinary";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
+import notificationRoutes from "./routes/notification.route.js";
 
 import connectMongoDB from './db/connectMongoDB.js';
 
@@ -30,6 +31,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 
 
