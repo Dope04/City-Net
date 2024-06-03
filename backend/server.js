254736e5269c61d713055807e798bafe807a96e1
Soumyadeep Mondal <142522496+soumyadeep1143@users.noreply.github.com>
2024-06-03 17:00:27 +0530

    backend is hectic

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
 
 import authRoutes from "./routes/auth.route.js";
-import connectMongoDB from './db/connectMongoDB.js'
-import cookieParser from "cookie-parser";
+import userRoutes from "./routes/user.route.js";
+
+import connectMongoDB from './db/connectMongoDB.js';
+
 
 dotenv.config();
+
+cloudinary.config({
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -17,6 +27,7 @@ app.use(cookieParser());
 
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 
 
