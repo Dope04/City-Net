e283d2341f82fcc35fdf461d515caa9d793c3f27
Soumyadeep Mondal <142522496+soumyadeep1143@users.noreply.github.com>
2024-07-02 21:24:42 +0530

    final

---
+++ backend/routes/post.route.js
@@ -0,0 +1,29 @@
+import express from "express";
+import { protectRoute } from "../middleware/protectRoute.js";
+import {
+  commentOnPost,
+  createPost,
+  deletePost,
+  getAllPosts,
+  getFollowingPosts,
+  getLikedPosts,
+  getUserPosts,
+  likeUnlikePost,
+  repostPost,
+  getAllCategory
+} from "../controllers/post.controller.js";
+
+const router = express.Router();
+
+router.get("/all", protectRoute, getAllPosts);
+router.get("/following", protectRoute, getFollowingPosts);
+router.get("/likes/:id", protectRoute, getLikedPosts);
+router.get("/user/:username", protectRoute, getUserPosts);
+router.post("/create", protectRoute, createPost);
+router.post("/like/:id", protectRoute, likeUnlikePost);
+router.post("/comment/:id", protectRoute, commentOnPost);
+router.post("/repost/:id", protectRoute, repostPost); 
+router.delete("/:id", protectRoute, deletePost);
+router.get("/category/:type", protectRoute, getAllCategory);
+
+export default router;
